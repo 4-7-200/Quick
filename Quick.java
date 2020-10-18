fea414deb7905bece4931646e246ea3d50d7fcf2
4-7-2000 <Chungku2001jr@gmail.com>
2020-10-18 19:32:31 +0600

    QuickSort

---
+++ Quick.java
@@ -0,0 +1,107 @@
+public class Quick 
+{
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(int[] a) 
+    {
+        sort(a, 0, a.length - 1);
+    }
+    // quicksort the subarray from a[lo] to a[hi]
+    private static void sort(int[] a, int lo, int hi) 
+    { 
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
+    // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+    // and return the index j.
+    private static int partition(int[] a, int lo, int hi) 
+    {
+        int i = lo;
+        int j = hi + 1;
+        int v = a[lo];
+        while (true) 
+        { 
+            while (less(a[++i], v))
+                {
+                    if (i == hi) break;
+                }
+            while (less(v, a[--j]))
+                {
+                    if (j == lo) break; 
+                }
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+
+    }
+
+   /***************************************************************************
+    *  Helper sorting functions.
+    ***************************************************************************/
+    
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w)
+     {
+        if (v == w) return false; 
+        return v.compareTo(w) < 0; 
+     }
+     // exchange a[i] and a[j]
+    private static void exch(int[] a, int i, int j) 
+    {
+        int swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;  
+    }
+
+
+   /***************************************************************************
+    *  Check if array is sorted - useful for debugging.
+    ***************************************************************************/
+    private static boolean isSorted(int[] a) 
+    {
+        return isSorted(a, 0, a.length - 1);   
+    }
+    private static boolean isSorted(int[] a, int lo, int hi) 
+    {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;  
+    }
+    // print array to standard output
+    private static void show(int[] a)
+     {
+        for (int i=0; i<a.length ;i++ ) 
+        {
+            System.out.print(a[i]);  
+        }
+    }
+    /**
+     * Reads in a sequence of values ; quicksorts them; 
+     * and prints them to standard output in ascending order. 
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int[] a = {1,9,3,6,8,2,7};
+        System.out.println("Unsorted number:");
+        for (int i = 0; i < a.length; i++) 
+        {
+            System.out.print(a[i]);
+        } 
+        Quick.sort(a);
+        System.out.println("");
+        System.out.println("Sorted number: ");
+        show(a);
+        System.out.println("");
+       assert isSorted(a);
+
+    }
+
+}
